e9a132c62d4823b6874d47063602b4dbe38e7b0b
abhi <abhishekgupta942001@gmail.com>
2021-08-04 20:38:55 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Google-Clone
+It's a Google Search Home Page Clone made using HTML5, CSS3, Bootstrap and JavaScript.
---
+++ clone.js
@@ -0,0 +1,20 @@
+function changer()
+{
+    var searchbox=document.querySelector(".search-box");
+    var image=document.querySelector('.apps');
+    if(document.body.style.backgroundColor=="")
+    {
+        document.body.style.backgroundColor="#000";
+        searchbox.style.backgroundColor="#000";
+        searchbox.style.color="#fff";
+        image.src="https://img.freepik.com/free-icon/nine-dots-in-a-square_318-67402.jpg?size=338&ext=jpg";
+    }
+    else
+    {
+        document.body.style.backgroundColor="";
+        searchbox.style.backgroundColor="";
+        searchbox.style.color="#000";
+        image.src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/3x3dot-black.svg/768px-3x3dot-black.svg.png";
+    }
+    
+}
